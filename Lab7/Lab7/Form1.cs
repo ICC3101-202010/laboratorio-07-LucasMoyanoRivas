fae341a37549b0622c33ade35650e16276e9352a
unknown <ljmoyano@miuandes.cl>
2020-05-22 15:04:25 -0400

    casi listo, solo falta ans y historial

--- Lab7/Lab7/Form1.cs
+++ Lab7/Lab7/Form1.cs
@@ -12,9 +12,69 @@ namespace Lab7
 {
     public partial class Form1 : Form
     {
+        Double valor = 0;
+        String operacion = "";
+        bool operacion_botonpresionado = false;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button_Click(object sender, EventArgs e)
+        {
+            if (Resultado.Text == "0" || operacion_botonpresionado) 
+            {
+                Resultado.Clear();
+            }
+            operacion_botonpresionado = false;
+            Button boton =(Button)sender;
+            Resultado.Text = Resultado.Text + boton.Text;
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            Resultado.Text = "0";
+        }
+
+        private void Operador_click(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            operacion = boton.Text;
+            valor = Double.Parse(Resultado.Text);
+            operacion_botonpresionado = true;
+            ecuacion.Text = valor + " " + operacion;
+        }
+        private void button18_Click(object sender, EventArgs e)
+        {
+            
+            ecuacion.Text = "";
+            switch(operacion)
+            {
+                case "+":
+                    Resultado.Text = (valor + Double.Parse(Resultado.Text)).ToString();
+                    break;
+                case "-":
+                    Resultado.Text = (valor - Double.Parse(Resultado.Text)).ToString();
+                    break;
+                case "x":
+                    Resultado.Text = (valor * Double.Parse(Resultado.Text)).ToString();
+                    break;
+                case "/":
+                    Resultado.Text = (valor / Double.Parse(Resultado.Text)).ToString();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            Resultado.Text = "0";
+            valor = 0;
         }
     }
 }
